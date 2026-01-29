9a9d0175875e00df1dc3e4c1153cee5e18274156
NLIPPI <nlippilira@gmail.com>
2026-01-29 00:03:28 -0300

    feat: animaciones scroll con Intersection Observer

--- js/main.js
+++ js/main.js
@@ -16,3 +16,23 @@ toggleBtn.addEventListener("click", () => {
   localStorage.setItem("theme", theme);
   toggleBtn.textContent = theme === "dark" ? "☀️" : "🌙";
 });
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+reveals.forEach((section) => {
+  observer.observe(section);
+});
